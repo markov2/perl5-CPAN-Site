f9172c6b14db41fb4465a78b02c2d4a392c99a69
Mark Overmeer <mark@overmeer.net>
2022-06-23 10:30:15 +0200

    Refer CPAN::Site to cpansite manual page

--- lib/CPAN/Site.pm
+++ lib/CPAN/Site.pm
@@ -3,4 +3,15 @@ use strict;
 
 package CPAN::Site;
 
+=head1 NAME
+
+CPAN::Site - extend CPAN with private packages
+
+=head1 SYNOPSIS
+
+You use this module via the script "cpansite", which gets
+installed when you install the perl modules.
+
+=cut
+
 1;
